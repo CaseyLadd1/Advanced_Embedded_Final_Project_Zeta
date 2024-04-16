69526e3fab1db04ab26d6d03d0afc71554ded789
Paul Karhnak @ Ubuntu WSL <zxz2hm@virginia.edu>
2024-04-16 16:15:41 -0400

    Added subdirectory for RNG; preliminary testing verifies RNG usefulness.

---
+++ rng/rng.c
@@ -0,0 +1,40 @@
+#include "rng.h"
+
+/**
+ * A simple XORshift pseudorandom number generator (PRNG) with theoretical period 2**32 - 1
+ * From: Marsaglia, George, 2003: "Xorshift RNGs"
+ * Retrieved from: <doi:10.18637/jss.v008.i14>
+ *
+ * Implemented by: Paul Karhnak (zxz2hm)
+ * For: ECE 4501: Advanced Embedded Computing Systems, Spring 2024
+ */
+uint32_t rng(void) {
+    static uint32_t CurrentRandomValue = 0;
+    
+    if (CurrentRandomValue == 0) {
+	uint32_t CurrentTimeSeed = 0;
+	while (CurrentTimeSeed == 0) {
+	    CurrentTimeSeed = OS_Time(); // Ensure a nonzero seed (which an XOR shift generator requires)
+	}
+	CurrentRandomValue = CurrentTimeSeed;
+    }
+
+    CurrentRandomValue ^= (CurrentRandomValue << 5);
+    CurrentRandomValue ^= (CurrentRandomValue >> 27);
+    CurrentRandomValue ^= (CurrentRandomValue << 25);
+
+    return CurrentRandomValue;
+}
+
+/**
+ * A helper utility to get a useful direction (0, 1, 2, 3, or 4)
+ * from a generated random number.
+ *
+ * Functionally, a wrapper for rng().
+ *
+ * Author: Paul Karhnak (zxz2hm)
+ * For: ECE 4501: Advanced Embedded Computing Systems, Spring 2024
+ */
+uint32_t GetRandomDirection(void) {
+    return rng() & ((uint32_t) 0x03);
+}
---
+++ rng/rng.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+#ifndef __RNG_H__
+#define __RNG_H__
+
+uint32_t rng(void);
+
+uint32_t GetRandomDirection(void);
+
+#endif
